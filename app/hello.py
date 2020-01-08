614ee35386adb17f3c942d0590cce131c5e713a7
Julien PC <j.perier1@gmail.com>
2020-01-08 17:50:29 +0100

    dummy json response

--- app/hello.py
+++ app/hello.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
-
+import json
 import platform
 import subprocess
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -23,6 +24,12 @@ def tag():
     p = subprocess.Popen(['git', 'describe', '--tags', '--abbrev=0'], stdout=subprocess.PIPE)
     p.wait()
     return p.stdout.read()
+
+@app.route("/user")
+def user():
+    with open('static/users.json', 'r') as f:
+        d = json.load(f)
+    return jsonify(d)
 
 if __name__ == "__main__":
     app.run()
