06fdd291efc511711be05d987e1dc1b1fd8fe5c1
Julien PC <j.perier1@gmail.com>
2020-01-15 11:22:16 +0100

    base du serveur étape 1 + connexion bd

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 # -*- coding:utf-8 -*-
 import json
-import platform
-import subprocess
 from flask import Flask, Response, request, jsonify
+import mysql.connector
+
+cnx = mysql.connector.connect(user='uxmafubcms8efnkd',
+                              password='uKyZrSTyH2NCsjaqxq7U',
+                              host='bwku7xxv8kkyfmuzbjf9-mysql.services.clever-cloud.com',
+                              database='bwku7xxv8kkyfmuzbjf9',
+                              port=3306)
 
 app = Flask(__name__)
 
@@ -12,13 +17,26 @@ def headers():
     return '<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()])
 
 
-@app.route("/user")
-def user():
+@app.route("/user", methods=['GET'])
+def get_users():
     with open('static/users.json', 'r') as f:
         d = json.load(f)
     return jsonify(d)
 
 
+@app.route('/user', methods=['PUT'])
+def put_users():
+    # TODO
+    return
+
+
+@app.route('/user', methods=['DELETE'])
+def delete_users():
+    # TODO
+    return "OK", 200
+
+
 if __name__ == "__main__":
     app.run()
+    cnx.close()
 
