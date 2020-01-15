bebcf3f8db903d90c9238a09e631d150592e0a31
Maganik <valentingrgrandjean@gmail.com>
2020-01-15 11:58:36 +0100

    Clarification

--- persistence.py
+++ persistence.py
@@ -11,11 +11,11 @@ class Persistence:
         cursor = self.cnx.cursor()
         cursor.execute(query)
         users = []
-        for (id, firstname, lastname, birthday) in cursor:
-            u = {"id" : id,
-                 "firstName" : firstname,
-                 "lastName" : lastname,
-                 "birthDay" : birthday}
+        for (id, firstName, lastName, birthday) in cursor:
+            u = {"id": id,
+                 "firstName": firstName,
+                 "lastName": lastName,
+                 "birthDay": birthday}
             users.append(u)
         return users
 
