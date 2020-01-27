3b2e2c06050de75cbeb9cf5ee7794057f2a9fa9a
Julien PC <j.perier1@gmail.com>
2020-01-27 15:39:13 +0100

    PUT, GET, DELETE routes

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 import json
-from flask import Flask, Response, request, jsonify
+from flask import Flask, make_response, request, jsonify
 import persistence as p
 import mysql.connector
 
@@ -12,9 +12,10 @@ cnx = mysql.connector.connect(user='uxmafubcms8efnkd',
 
 app = Flask(__name__)
 
-pers = p.Persistence(cnx)
+persistence = p.Persistence(cnx)
 
 
+# TODO: Remove ?
 @app.route("/")
 def headers():
     return '<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()])
@@ -23,20 +24,65 @@ def headers():
 @app.route("/user", methods=['GET'])
 def get_users():
     page = request.args.get('page', default=0, type=int)
-    users = pers.get_users(page)
+    users = persistence.get_users()
     return jsonify(users)
+
+
+@app.route("/user/<uid>", methods=['GET'])
+def get_user(uid):
+    user = persistence.get_user(uid)
+    return jsonify(user)
 
 
 @app.route('/user', methods=['PUT'])
 def put_users():
-    # TODO
-    return
+    # TODO: vérifier qu'il a bien un id ? vérifier que c'est bien une list ?
+    user_list = [
+        [
+            u['id'],
+            u['firstName'],
+            u['lastName'],
+            u['birthDay']
+        ]
+        for u in list(request.json)
+    ]
+    if persistence.put_users(user_list):
+        return make_response("OK", 201)
+    else:
+        return make_response("BAD", 500)
+
+
+@app.route('/user/<uid>', methods=['PUT'])
+def put_user(uid):
+    user = request.json
+    try:
+        _ = user['id']
+    except KeyError:
+        user['id'] = uid
+    if persistence.put_user(user):
+        return make_response("OK", 201)
+    else:
+        return make_response("BAD", 500)
 
 
 @app.route('/user', methods=['DELETE'])
 def delete_users():
-    # TODO
-    return "OK", 200
+    persistence.delete_users()
+    return make_response("OK", 200)
+
+
+@app.route('/user/<uid>', methods=['DELETE'])
+def delete_users(uid):
+    if persistence.delete_user(uid):
+        return make_response("OK", 200)
+    else:
+        return make_response("BAD", 500)
+
+
+@app.route("/user", methods=['POST'])
+def post_user():
+    persistence.post_user(request.json)
+    return make_response("OK", 200)
 
 
 if __name__ == "__main__":
